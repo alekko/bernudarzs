40076966ed383a88625fa21ec4c013a4f5963f7e
Aleksandrs Kozlovs <aleksandrs.kozlovs@makit.lv>
2019-06-04 02:33:27 +0300

    demo

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,12 +46,12 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => public_path(),
+            'visibility' => 'public',
         ],
 
         'public' => [
             'driver' => 'local',
             'root' => public_path(),
-            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
 
