a3de180153c9f60bc0f7b9482f1d2d57d60e677a
Aleksandrs Kozlovs <aleksandrs.kozlovs@makit.lv>
2019-06-03 23:18:40 +0300

    nova gate update

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,9 +42,13 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            if ($user->hasPermission('Nova')) {
+                return true;
+            }
+
+            // If user doesn't have access to nova, log them out.
+            // This prevents them for being stuck in 403 page.
+            Auth::logout();
         });
     }
 
