63b8b6d7efe049541b020b2760873dfca29140ae
Aleksandrs Kozlovs <aleksandrs.kozlovs@makit.lv>
2019-06-04 19:40:50 +0300

    secure url

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -10,10 +10,10 @@
 <!-- Nav -->
 <nav id="menu">
   <ul class="links">
-    <li><a href="{{route('home')}}">Sākums</a></li> <!-- 1 -->
-    <li><a href="{{route('about')}}">Par Mums</a></li> <!-- 2 -->
-    <li><a href="{{route('timetable')}}">Laika grafiks</a></li> <!-- 3 -->
-    <li><a href="{{route('contact')}}">Kontakti</a></li> <!-- 4 -->
+    <li><a href="{{secure_url(route('home'))}}">Sākums</a></li> <!-- 1 -->
+    <li><a href="{{secure_url(route('about'))}}">Par Mums</a></li> <!-- 2 -->
+    <li><a href="{{secure_url(route('timetable'))}}">Laika grafiks</a></li> <!-- 3 -->
+    <li><a href="{{secure_url(route('contact'))}}">Kontakti</a></li> <!-- 4 -->
     <li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
   </ul>
 </nav>
