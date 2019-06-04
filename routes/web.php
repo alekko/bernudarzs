3c52a7bf89ecca316b3da08190d6b4fe5c37ec00
Aleksandrs Kozlovs <aleksandrs.kozlovs@makit.lv>
2019-06-04 19:43:26 +0300

    routes update

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+
+URL::forceSchema('https');
+
+
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/about', 'AboutController@index')->name('about');
 Route::get('/contact', 'ContactController@index')->name('contact');
