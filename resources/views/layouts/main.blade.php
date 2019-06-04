1b4393fce90bb89a59569f92ef2fb83c85ef5c56
Aleksandrs Kozlovs <aleksandrs.kozlovs@makit.lv>
2019-06-04 03:16:40 +0300

    https

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,8 +7,8 @@
         <meta name="description" content="">
         <meta name="keywords" content="">
         <link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}?s=2">
+        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
         <script src="{{ URL::asset('js/app.js') }}"></script>
     </head>
     <body class="{{ Route::currentRouteName() != 'home' ? 'subpage' : '' }}">
