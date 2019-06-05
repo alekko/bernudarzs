a4d3ea0571f8c9deb167e2e3ea2c16914c2d6699
Aleksandrs Kozlovs <aleksandrs.kozlovs@makit.lv>
2019-06-06 01:11:02 +0300

    updates

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'APP_URL=https://bernudarzs.herokuapp.com'),
+    'url' => env('APP_URL', 'https://bernudarzs.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
